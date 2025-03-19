25db80b5d3e4050977dd612e19e4f19bcdbfedf4
Lynzzyr <brandonnamgoong@gmail.com>
2025-03-19 01:22:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# banking-emulator
+ Java port of ICS3U Python assignment
---
+++ src/main/java/com/lynzzyr/ics3u/atm/Main.java
@@ -0,0 +1,7 @@
+package com.lynzzyr.ics3u.atm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
